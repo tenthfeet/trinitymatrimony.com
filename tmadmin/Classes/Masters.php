f5776a9c90cf5f9dd8a3b81b91e6c2f177d1ba5c
tenthfeet <94008981+tenthfeet@users.noreply.github.com>
2022-01-10 13:36:46 +0530

    Add files via upload

---
+++ tmadmin/Classes/Masters.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Developed by tenthfeet.com
+ */
+class Class_Masters
+{
+	/**
+	 * @var contain database connection
+	 */
+	protected $_con;
+	/**
+	 * it will initalize DBclass
+	 */
+	public function __construct()
+	{
+		$db = new Class_DBcon();
+		$this->_con = $db->con;
+        global $modDate, $curDate;
+        $modDate = $curDate  = date('Y-m-d');
+	}
+
+  
+
+}//Class end
